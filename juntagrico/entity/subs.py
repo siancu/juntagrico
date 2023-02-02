5358087f1ed9202361996b8175f37bf6442379b8
pascalfree <pascal.free@ihdg.ch>
2023-02-03 00:08:59 +0100

    do assignments calculations on the database & fix counting bugs

--- juntagrico/entity/subs.py
+++ juntagrico/entity/subs.py
@@ -13,8 +13,9 @@ from juntagrico.entity.member import q_left_subscription, q_joined_subscription
 from juntagrico.lifecycle.sub import check_sub_consistency
 from juntagrico.lifecycle.subpart import check_sub_part_consistency
 from juntagrico.mailer import membernotification
+from juntagrico.queryset.subscription import SubscriptionQuerySet
 from juntagrico.util.models import q_activated, q_cancelled, q_deactivated, q_deactivation_planned, q_isactive
-from juntagrico.util.temporal import start_of_next_business_year, start_of_business_year, end_of_business_year
+from juntagrico.util.temporal import start_of_next_business_year
 
 
 class Subscription(Billable, SimpleStateModel):
@@ -40,6 +41,8 @@ class Subscription(Billable, SimpleStateModel):
     notes = models.TextField(
         _('Notizen'), max_length=1000, blank=True,
         help_text=_('Notizen für Administration. Nicht sichtbar für {}'.format(Config.vocabulary('member'))))
+
+    objects = SubscriptionQuerySet.as_manager()
 
     def __str__(self):
         return _('Abo ({1}) {0}').format(self.size, self.id)
@@ -78,11 +81,6 @@ class Subscription(Billable, SimpleStateModel):
     @property
     def active_and_future_parts(self):
         return self.parts.filter(~q_deactivated())
-
-    @property
-    def parts_in_business_year(self):
-        return self.parts.filter(Q(activation_date__isnull=False, activation_date__lte=end_of_business_year()) &
-                                 ~Q(deactivation_date__isnull=False, deactivation_date__lt=start_of_business_year()))
 
     @property
     def part_change_date(self):
@@ -124,16 +122,12 @@ class Subscription(Billable, SimpleStateModel):
     def subscription_amount_future(self, size):
         return self.calc_subscription_amount(self.future_parts, size)
 
-    @property
-    def required_assignments(self):
-        return self.get_required_assignments()
-
-    @property
-    def required_core_assignments(self):
-        return self.get_required_assignments(True)
-
-    def get_required_assignments(self, core=False):
-        return round(sum([part.get_required_assignments(core) for part in self.parts_in_business_year.select_related('type')]))
+    def get_with_assignments(self, *args, **kwargs):
+        """
+        calculate assignments of this sub and write result into properties.
+        If applying this on multiple elements use `annotate_assignments()` on queryset instead.
+        """
+        return Subscription.objects.annotate_assignments(*args, **kwargs).get(pk=self)
 
     @property
     def price(self):
@@ -280,23 +274,6 @@ class SubscriptionPart(JuntagricoBaseModel, SimpleStateModel):
     @property
     def is_extra(self):
         return self.type.size.product.is_extra
-
-    def get_required_assignments(self, core=False):
-        if not self.activation_date:
-            return 0
-        nominal_required = self.type.required_core_assignments if core else self.type.required_assignments
-        # on trial subs no discount applies
-        if self.type.trial_days:
-            return nominal_required
-        # since when part is active in current business year
-        start_business = start_of_business_year()
-        start = max(self.activation_date, start_business)
-        # when (if at all) part will be inactive in current business year
-        end_business = end = end_of_business_year()
-        if self.deactivation_date:
-            end = min(self.deactivation_date, end_business)
-        # percentage of business year, where part is active. Do not round here.
-        return nominal_required * (end - start) / (end_business - start_business)
 
     def clean(self):
         check_sub_part_consistency(self)
--- test/test_assignments.py
+++ test/test_assignments.py
@@ -1,62 +1,56 @@
 from datetime import date
 
-from django.utils import timezone
-
-from juntagrico.entity.subs import SubscriptionPart, Subscription
-from juntagrico.entity.subtypes import SubscriptionType
+from juntagrico.entity.subs import Subscription
 from test.util.test import JuntagricoTestCase
 
 
 class AssignmentTests(JuntagricoTestCase):
+    year = 2022
+    activation_date = date(day=1, month=1, year=year)
 
     def setUp(self):
         super().setUp()
-        sub_type_data = {
-            'name': 'sub_trial_type',
-            'long_name': 'sub_type_long_name',
-            'size': self.sub_size,
-            'shares': 2,
-            'visible': True,
-            'required_assignments': 10,
-            'required_core_assignments': 3,
-            'price': 1000,
-            'description': 'sub_type_desc'}
-        self.sub_trial_type = SubscriptionType.objects.create(trial_days=30, **sub_type_data)
+        self.sub_trial_type = self.create_sub_type(self.sub_size, trial_days=30)
 
-        year = timezone.now().year
-        activation_date = date(day=1, month=7, year=year)
-        self.sub2.activation_date = activation_date
-        SubscriptionPart.objects.create(subscription=self.sub2, type=self.sub_type, activation_date=activation_date)
-        activation_date = date(day=1, month=1, year=year)
-        deactivation_date = date(day=30, month=6, year=year)
-        sub_data = {
-            'depot': self.depot,
-            'future_depot': None,
-            'creation_date': '2017-03-27',
-            'start_date': '2018-01-01'
-        }
-        dates = {
-            'activation_date': activation_date,
-            'cancellation_date': activation_date,
-            'deactivation_date': deactivation_date,
-        }
-        sub_data3 = {**sub_data, **dates}
-        # sub in first half of the year
-        self.sub3 = Subscription.objects.create(**sub_data3)
-        SubscriptionPart.objects.create(subscription=self.sub3, type=self.sub_type, **dates)
-        # trial sub
-        self.sub4 = Subscription.objects.create(**sub_data3)
-        SubscriptionPart.objects.create(subscription=self.sub4, type=self.sub_trial_type, **dates)
-        # ordered, not activated sub
-        self.sub5 = Subscription.objects.create(**sub_data)
-        SubscriptionPart.objects.create(subscription=self.sub5, type=self.sub_type)
+        self.subs = [
+            # sub in second half of the year
+            self.create_sub(self.depot, date(day=1, month=7, year=self.year), parts=[self.sub_type]),
+            # sub in first half of the year
+            self.create_sub(self.depot, self.activation_date, deactivation_date=date(day=30, month=6, year=self.year), parts=[self.sub_type]),
+            # trial sub ongoing
+            self.create_sub(self.depot, self.activation_date, [self.sub_trial_type]),
+            # trial sub shorter than planned
+            self.create_sub(self.depot, self.activation_date, deactivation_date=date(day=15, month=1, year=self.year), parts=[self.sub_trial_type]),
+            # trial sub at the end of the year
+            self.create_sub(self.depot, date(day=15, month=12, year=self.year), [self.sub_trial_type]),
+            # trial sub starting last year
+            self.create_sub(self.depot, date(day=15, month=12, year=self.year - 1), [self.sub_trial_type]),
+            # ordered, not activated sub
+            self.create_sub(self.depot, parts=[self.sub_type]),
+        ]
 
     def testRequiredAssignments(self):
-        self.assertEqual(self.sub2.required_assignments, 5)
-        self.assertEqual(self.sub2.required_core_assignments, 2)
-        self.assertEqual(self.sub3.required_assignments, 5)
-        self.assertEqual(self.sub3.required_core_assignments, 1)  # first 6 months or the year are a bit shorter
-        self.assertEqual(self.sub4.required_assignments, 10)
-        self.assertEqual(self.sub4.required_core_assignments, 3)
-        self.assertEqual(self.sub5.required_assignments, 0)
-        self.assertEqual(self.sub5.required_core_assignments, 0)
+        # get assignments for entire year.
+        subs = Subscription.objects.annotate_required_assignments(self.activation_date, date(self.year, 12, 31)).filter(pk__in=self.subs)
+        # sub in second half of the year
+        self.assertEqual(subs[0].required_assignments, 5)
+        self.assertEqual(subs[0].required_core_assignments, 2)
+        # sub in first half of the year
+        self.assertEqual(subs[1].required_assignments, 5)
+        self.assertEqual(subs[1].required_core_assignments, 1)  # first 6 months or the year are a bit shorter
+        # trial sub ongoing
+        self.assertEqual(subs[2].required_assignments, 10)
+        self.assertEqual(subs[2].required_core_assignments, 3)
+        # trial sub shorter than normal trial period
+        self.assertEqual(subs[3].required_assignments, 5)
+        self.assertEqual(subs[3].required_core_assignments, 2)
+        # trial sub at the end of the year
+        self.assertEqual(subs[4].required_assignments, 6)  # 17/30 rounded
+        self.assertEqual(subs[4].required_core_assignments, 2)
+        # trial sub starting last year
+        self.assertEqual(subs[5].required_assignments, 4)  # 13/30 rounded
+        self.assertEqual(subs[5].required_core_assignments, 1)
+        # ordered, not activated sub
+        self.assertEqual(subs[6].required_assignments, 0)
+        self.assertEqual(subs[6].required_core_assignments, 0)
+        # TODO: test subscriptions with multiple parts
